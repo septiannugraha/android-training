a402b8b604050d365047f8cfc1c925ba06508073
Septian Adi Nugraha <septian.adi.nugraha@gmail.com>
2019-07-31 11:10:50 +0700

    Day 3 part 1

--- app/src/main/java/id/go/bpkp/training/MainActivity.kt
+++ app/src/main/java/id/go/bpkp/training/MainActivity.kt
@@ -2,8 +2,12 @@ package id.go.bpkp.training
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import id.go.bpkp.training.dummy.DummyContent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SuratTugasFragment.OnListFragmentInteractionListener {
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/id/go/bpkp/training/SplashActivity.kt
+++ app/src/main/java/id/go/bpkp/training/SplashActivity.kt
@@ -4,15 +4,19 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import id.go.bpkp.training.dummy.DummyContent
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), SuratTugasFragment.OnListFragmentInteractionListener {
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        Handler().postDelayed( { startActivity(Intent(this,HomeActivity::class.java))
+        Handler().postDelayed( { startActivity(Intent(this, MainActivity::class.java))
             // close this activity
             finish()
-        }, 3000)
+        }, 1500)
     }
 }
